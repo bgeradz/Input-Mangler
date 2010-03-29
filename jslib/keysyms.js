a5b54ef9c0d5309892cfcea6ca545d724708c683
Genadz Batsyan <gbatyan@gmail.com>
2010-03-29 21:18:03 +0300

    Added led syms to keysyms.js (temporarily).

--- jslib/keysyms.js
+++ jslib/keysyms.js
@@ -277,6 +277,23 @@ KEY_KBDILLUMUP		 = 230;
 KEY_UNKNOWN		 = 240;
 
 
+/* Leds */
+LED_NUML              =  0x00
+LED_CAPSL             =  0x01
+LED_SCROLLL           =  0x02
+LED_COMPOSE           =  0x03
+LED_KANA              =  0x04
+LED_SLEEP             =  0x05
+LED_SUSPEND           =  0x06
+LED_MUTE              =  0x07
+LED_MISC              =  0x08
+LED_MAIL              =  0x09
+LED_CHARGING          =  0x0a
+LED_MAX               =  0x0f
+LED_CNT               =  (LED_MAX+1)
+
+
+
 /* Constants used by some scripts */
 SHIFT = 1000; // left/right shift
 
